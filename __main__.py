a86e8229fae4ef7d026984c988a05965feec0ee7
EC2 Default User <ec2-user@ip-172-31-27-248.ec2.internal>
2021-11-20 10:01:59 +0000

    fix: userdata

--- __main__.py
+++ __main__.py
@@ -89,12 +89,15 @@ pulumi.export('backend public_ip', backend.public_ip)
 
 userdata_frontend = pulumi.Output.all([backend.public_ip]).apply(lambda args:
     """#!/bin/bash
-    # install maven
+    # install 
+    sudo yum update -y
+    sudo yum install java-11-amazon-corretto -y
     sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
     sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
     sudo yum install -y apache-maven
     sudo yum install java-1.8.0-devel -y
     sudo update-alternatives --set java /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java
+    sudo yum install -y git
     
     # run
     git clone https://github.com/andy-ecloud/cloud-edge-lab-template.git /cloud-edge-lab-template
--- userdata_backend.sh
+++ userdata_backend.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # install maven
+sudo yummm update -y
+sudo yum install java-11-amazon-corretto -y
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
 sudo yum install java-1.8.0-devel -y
-sudo update-alternatives --set java /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java
 
+# java 
+sudo yum install java-11-amazon-corretto-headless
+sudo yum install java-11-amazon-corretto
+
+sudo update-alternatives --set java /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java
+sudo yum install -y git
+    
 # run
+
 git clone https://github.com/andy-ecloud/cloud-edge-lab-template.git /cloud-edge-lab-template
 mvn install -f /cloud-edge-lab-template/api-parent/pom.xml
 mvn install -f /cloud-edge-lab-template/api-backend/pom.xml
