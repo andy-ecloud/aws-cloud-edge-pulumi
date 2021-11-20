6b755557da123e1a5842aa3d5435ffff8c13f0f9
EC2 Default User <ec2-user@ip-172-31-27-248.ec2.internal>
2021-11-20 08:58:39 +0000

    fix: userdat

--- userdata_backend.sh
+++ userdata_backend.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # install maven
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
